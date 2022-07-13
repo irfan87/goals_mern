68042189d620412e822af0cfa6431426fffff992
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2022-07-14 02:06:26 +0800

    accepting body data and setup error handling middleware

--- backend/controllers/GoalController.js
+++ backend/controllers/GoalController.js
@@ -9,6 +9,13 @@ const getGoals = (req, res) => {
 // @route 	POST /api/goals
 // @access 	Private
 const setGoal = (req, res) => {
+	if (!req.body.text) {
+		res.status(400);
+
+		throw new Error("No goal provided");
+	}
+
+	console.log(req.body);
 	res.status(200).json({ message: "POST Goals from GoalController.js" });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const { errorHandler } = require("./middleware/ErrorMiddleware");
 const dotenv = require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+
 app.use("/api/goals", require("./routes/GoalRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
