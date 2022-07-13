8960153ee32e0ab95dc99948c19293b5c65d97ab
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2022-07-13 23:58:38 +0800

    setup routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/goals", require("./routes/GoalRoutes"));
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
